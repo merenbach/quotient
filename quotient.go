5701adfd07994e1819c8cfc855791ecf59cbd15e
Andrew Merenbach <andrew@merenbach.com>
2018-03-28 20:00:16 -0700

    Add basic Golang version

---
+++ quotient.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// Radix holds the base in which division operations are performed.
+const radix = 10
+
+// DivMod returns the integer quotient and remainder of a fraction.
+func DivMod(a, b int64) (q, r int64) {
+	q, r = a/b, a%b
+	return
+}
+
+// Divide divides one number by another out to particular number of decimal places.
+func Divide(a, b, scale int64) {
+	for i := int64(0); i < scale; i++ {
+		q, r := DivMod(a, b)
+		fmt.Printf("%d", q)
+		if i == 0 {
+			fmt.Print(".")
+		}
+		a = r * radix
+	}
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("Please enter args")
+	}
+
+	a, _ := strconv.ParseInt(os.Args[1], radix, 64)
+	b, _ := strconv.ParseInt(os.Args[2], radix, 64)
+	scale, _ := strconv.ParseInt(os.Args[3], 10, 64)
+	Divide(a, b, scale)
+}
